32ceb235a85cdc136cd080bf045fc09fd3e1fb55
Javier Hidalgo <javier.hidalgo_carrio@dfki.de>
2015-02-10 21:01:19 +0100

    included odometry and imu gyros in the GP motion model

--- src/gaussian_processes/gpy_motion_model.py
+++ src/gaussian_processes/gpy_motion_model.py
@@ -36,6 +36,11 @@ reference_velocity = data.ThreeData()
 reference_velocity.readData(pose_ref_velocity_file, cov=True)
 reference_velocity.eigenValues()
 
+# Odometry Robot Velocity
+odometry_velocity = data.ThreeData()
+odometry_velocity.readData(pose_odo_velocity_file, cov=True)
+odometry_velocity.eigenValues()
+
 # IMU orientation
 imu_orient = data.QuaternionData()
 imu_orient.readData(pose_imu_orientation_file, cov=True)
@@ -45,6 +50,11 @@ imu_orient.eigenValues()
 imu_acc = data.ThreeData()
 imu_acc.readData(pose_imu_acceleration_file, cov=False)
 imu_acc.eigenValues()
+
+# IMU Angular Velocity
+imu_gyro = data.ThreeData()
+imu_gyro.readData(pose_imu_angular_velocity_file, cov=False)
+imu_gyro.eigenValues()
 
 # Robot Joints Position and Speed
 names = "left_passive", "fl_mimic", "fl_walking", "fl_steer", "fl_drive", "fl_contact", "fl_translation", "fl_slipx", "fl_slipy", "fl_slipz", "ml_mimic", "ml_walking", "ml_drive", "ml_contact", "ml_translation", "ml_slipx", "ml_slipy", "ml_slipz", "rear_passive", "rl_mimic", "rl_walking", "rl_steer", "rl_drive", "rl_contact", "rl_translation", "rl_slipx", "rl_slipy", "rl_slipz", "rr_mimic", "rr_walking", "rr_steer", "rr_drive", "rr_contact", "rr_translation", "rr_slipx", "rr_slipy", "rr_slipz", "right_passive", "fr_mimic", "fr_walking", "fr_steer", "fr_drive", "fr_contact", "fr_translation", "fr_slipx", "fr_slipy", "fr_slipz", "mr_mimic", "mr_walking", "mr_drive", "mr_contact", "mr_translation", "mr_slipx", "mr_slipy", "mr_slipz"
@@ -64,8 +74,8 @@ sample_rate = 1.0/delta_t
 nyq_rate = 0.5 * sample_rate
 
 # The cutoff frequency of the filter (in Hz)
-low_cut_hz = 2
-high_cut_hz = 2
+low_cut_hz = 1
+high_cut_hz = 1
 
 # Length of the filter (number of coefficients, i.e. the filter order + 1)
 filter_order = 8
@@ -106,7 +116,9 @@ joints = np.column_stack((
                         ))
 ##################
 # Inertia Inputs #
-inertia = np.column_stack((imu_acc.getAxis(0), imu_acc.getAxis(1), imu_acc.getAxis(2)))
+#inertia = np.column_stack((imu_acc.getAxis(0), imu_acc.getAxis(1), imu_acc.getAxis(2)))
+inertia = np.column_stack((imu_acc.getAxis(0), imu_acc.getAxis(1), imu_acc.getAxis(2),
+                           imu_gyro.getAxis(0), imu_gyro.getAxis(1), imu_gyro.getAxis(2)))
 
 ######################
 # Orientation Inputs #
@@ -145,6 +157,7 @@ orient = orient.astype('float32')
 # GP Multidimensional vector
 #X = np.column_stack((joints[:,0:2])) #Only two joints
 X = np.column_stack((joints, inertia, orient))
+#X = np.column_stack((joints, inertia)) #No orientation
 #X = np.column_stack((X))
 
 ########################
@@ -164,13 +177,16 @@ Y = np.column_stack(Y)
 # The flag ARD=True in the definition of the Matern kernel specifies that we
 # want one lengthscale parameter per dimension (ie the GP is not isotropic).
 #ker = GPy.kern.Matern52(X.shape[1], ARD=True)
-ker = GPy.kern.RBF(input_dim = X.shape[1], ARD=True)
+ker_rbf = GPy.kern.RBF(input_dim = X.shape[1], ARD=False)
+#ker_white = GPy.kern.White(input_dim = X.shape[1])
+#ker = ker_white + ker_rbf
+ker =  ker_rbf
 
 # create simple GP model
-m = GPy.models.GPRegression(X, Y, kernel=ker)
-#m = GPy.models.GPHeteroscedasticRegression(X, Y, kernel=ker)
-Z = np.array(np.row_stack((vectoridx, vectoridx)))
-Z = Z.astype(float32)
+#m = GPy.models.GPRegression(X, Y, kernel=ker)
+m = GPy.models.GPHeteroscedasticRegression(X, Y, kernel=ker)
+#Z = np.array(np.row_stack((vectoridx, vectoridx)))
+#Z = Z.astype(float32)
 #m = GPy.models.SparseGPRegression(X=X, Y=Y, kernel=ker, num_inducing=100)
 
 
@@ -293,10 +309,6 @@ reference_position = data.ThreeData()
 reference_position.readData(pose_ref_position_file, cov=True)
 reference_position.eigenValues()
 
-# Odometry Robot Velocity
-odometry_velocity = data.ThreeData()
-odometry_velocity.readData(pose_odo_velocity_file, cov=True)
-odometry_velocity.eigenValues()
 
 #Odometry Position comparison X-Time
 matplotlib.rcParams.update({'font.size': 30, 'font.weight': 'bold'})
@@ -317,7 +329,7 @@ plt.grid(True)
 ax.legend(loc=1, prop={'size':30})
 plt.show(block=False)
 
-#Reference Velocity comparison X-Time
+#Reference and GP Velocity comparison X-Time
 matplotlib.rcParams.update({'font.size': 30, 'font.weight': 'bold'})
 fig = plt.figure(1)
 ax = fig.add_subplot(111)
@@ -331,7 +343,7 @@ ax.plot(reference_velocity.time, xvelocity, marker='o', linestyle='-.', label="R
 xvelocity = mean1[:,0]
 xtime = np.array(reference_velocity.time)
 xtime = np.delete(xtime, randomvector, 0)
-ax.plot(xtime, xvelocity, marker='o', linestyle='-.', label="CumSum Odometry", color=[0,0.5,1.0], lw=2)
+ax.plot(xtime, xvelocity, marker='o', linestyle='-.', label="GP Velocity", color=[0,0.5,1.0], lw=2)
 
 plt.xlabel(r'X [$m$]', fontsize=35, fontweight='bold')
 plt.ylabel(r'Y [$m$]', fontsize=35, fontweight='bold')
@@ -339,7 +351,7 @@ plt.grid(True)
 ax.legend(loc=1, prop={'size':30})
 plt.show(block=False)
 
-#Reference Velocity comparison Z-Time
+#Reference and GP Velocity comparison Y-Time
 matplotlib.rcParams.update({'font.size': 30, 'font.weight': 'bold'})
 fig = plt.figure(1)
 ax = fig.add_subplot(111)
@@ -353,7 +365,7 @@ ax.plot(reference_velocity.time, yvelocity, marker='o', linestyle='-.', label="R
 yvelocity = mean1[:,1]
 ytime = np.array(reference_velocity.time)
 ytime = np.delete(ytime, randomvector, 0)
-ax.plot(ytime, yvelocity, marker='o', linestyle='-.', label="CumSum Odometry", color=[0,0.5,1.0], lw=2)
+ax.plot(ytime, yvelocity, marker='o', linestyle='-.', label="GP Velocity", color=[0,0.5,1.0], lw=2)
 
 plt.xlabel(r'X [$m$]', fontsize=35, fontweight='bold')
 plt.ylabel(r'Y [$m$]', fontsize=35, fontweight='bold')
@@ -361,7 +373,7 @@ plt.grid(True)
 ax.legend(loc=1, prop={'size':30})
 plt.show(block=False)
 
-#Reference Velocity comparison Z-Time
+#Reference and GP Velocity comparison Z-Time
 matplotlib.rcParams.update({'font.size': 30, 'font.weight': 'bold'})
 fig = plt.figure(1)
 ax = fig.add_subplot(111)
@@ -375,7 +387,7 @@ ax.plot(reference_velocity.time, zvelocity, marker='o', linestyle='-.', label="R
 zvelocity = mean1[:,2]
 ztime = np.array(reference_velocity.time)
 ztime = np.delete(ztime, randomvector, 0)
-ax.plot(ztime, zvelocity, marker='o', linestyle='-.', label="CumSum Odometry", color=[0,0.5,1.0], lw=2)
+ax.plot(ztime, zvelocity, marker='o', linestyle='-.', label="GP Velocity", color=[0,0.5,1.0], lw=2)
 
 plt.xlabel(r'X [$m$]', fontsize=35, fontweight='bold')
 plt.ylabel(r'Y [$m$]', fontsize=35, fontweight='bold')
@@ -383,7 +395,7 @@ plt.grid(True)
 ax.legend(loc=1, prop={'size':30})
 plt.show(block=False)
 
-#Position comparison X-Y plane
+#Odometry, Reference and GP Position comparison X-Y plane
 matplotlib.rcParams.update({'font.size': 30, 'font.weight': 'bold'})
 fig = plt.figure(1)
 ax = fig.add_subplot(111)
@@ -417,4 +429,138 @@ plt.ylabel(r'Y [$m$]', fontsize=35, fontweight='bold')
 plt.grid(True)
 ax.legend(loc=1, prop={'size':30})
 plt.show(block=False)
+
+#######################
+## LOAD ANOTHER TEST ##
+#######################
+joints_position_file = '/home/javi/exoter/development/post-process_data/20141024_planetary_lab/20141025-0005/joints_position.0.data'
+
+joints_speed_file = '/home/javi/exoter/development/post-process_data/20141024_planetary_lab/20141025-0005/joints_speed.0.data'
+
+pose_ref_velocity_file =  '/home/javi/exoter/development/post-process_data/20141024_planetary_lab/20141025-0005/pose_ref_velocity.0.data'
+
+pose_odo_velocity_file =  '/home/javi/exoter/development/post-process_data/20141024_planetary_lab/20141025-0005/pose_odo_velocity.0.data'
+
+pose_imu_orientation_file =  '/home/javi/exoter/development/post-process_data/20141024_planetary_lab/20141025-0005/pose_imu_orientation.0.data'
+
+pose_imu_angular_velocity_file =  '/home/javi/exoter/development/post-process_data/20141024_planetary_lab/20141025-0005/pose_imu_angular_velocity.0.data'
+
+pose_imu_acceleration_file =  '/home/javi/exoter/development/post-process_data/20141024_planetary_lab/20141025-0005/pose_imu_acceleration.0.data'
+#######################################
+
+# Reference Robot Velocity
+reference_velocity = data.ThreeData()
+reference_velocity.readData(pose_ref_velocity_file, cov=True)
+reference_velocity.eigenValues()
+
+# Odometry Robot Velocity
+odometry_velocity = data.ThreeData()
+odometry_velocity.readData(pose_odo_velocity_file, cov=True)
+odometry_velocity.eigenValues()
+
+# IMU orientation
+imu_orient = data.QuaternionData()
+imu_orient.readData(pose_imu_orientation_file, cov=True)
+imu_orient.eigenValues()
+
+# IMU acceleration
+imu_acc = data.ThreeData()
+imu_acc.readData(pose_imu_acceleration_file, cov=False)
+imu_acc.eigenValues()
+
+# IMU Angular Velocity
+imu_gyro = data.ThreeData()
+imu_gyro.readData(pose_imu_angular_velocity_file, cov=False)
+imu_gyro.eigenValues()
+
+# Robot Joints Position and Speed
+names = "left_passive", "fl_mimic", "fl_walking", "fl_steer", "fl_drive", "fl_contact", "fl_translation", "fl_slipx", "fl_slipy", "fl_slipz", "ml_mimic", "ml_walking", "ml_drive", "ml_contact", "ml_translation", "ml_slipx", "ml_slipy", "ml_slipz", "rear_passive", "rl_mimic", "rl_walking", "rl_steer", "rl_drive", "rl_contact", "rl_translation", "rl_slipx", "rl_slipy", "rl_slipz", "rr_mimic", "rr_walking", "rr_steer", "rr_drive", "rr_contact", "rr_translation", "rr_slipx", "rr_slipy", "rr_slipz", "right_passive", "fr_mimic", "fr_walking", "fr_steer", "fr_drive", "fr_contact", "fr_translation", "fr_slipx", "fr_slipy", "fr_slipz", "mr_mimic", "mr_walking", "mr_drive", "mr_contact", "mr_translation", "mr_slipx", "mr_slipy", "mr_slipz"
+
+robot_joints = js.Joints(names)
+robot_joints.readData(joints_position_file, joints_speed_file)
+
+#######################################
+# Form the New Inputs to predict
+# Joints Inputs #
+joints = np.column_stack((
+                        robot_joints.getSpeed("fl_translation"),
+                        robot_joints.getSpeed("fr_translation"),
+                        robot_joints.getSpeed("ml_translation"),
+                        robot_joints.getSpeed("mr_translation"),
+                        robot_joints.getSpeed("rl_translation"),
+                        robot_joints.getSpeed("rr_translation"),
+                        robot_joints.getPosition("fl_steer"),
+                        robot_joints.getPosition("fr_steer"),
+                        robot_joints.getPosition("rl_steer"),
+                        robot_joints.getPosition("rr_steer"),
+                        robot_joints.getPosition("left_passive"),
+                        robot_joints.getPosition("right_passive"),
+                        robot_joints.getPosition("rear_passive")
+                        ))
+##################
+# Inertia Inputs #
+inertia = np.column_stack((imu_acc.getAxis(0), imu_acc.getAxis(1), imu_acc.getAxis(2),
+                           imu_gyro.getAxis(0), imu_gyro.getAxis(1), imu_gyro.getAxis(2)))
+
+######################
+# Orientation Inputs #
+# Roll and Pitch angles in that order #
+orient = np.column_stack((imu_orient.getEuler(2), imu_orient.getEuler(1)))
+
+###################################################################
+# Create the random vector for sub sampling the whole data values #
+percentage = 2.0
+dimension = min (len(imu_acc.getAxis(0)), len(imu_acc.getAxis(1)))
+
+randomvector = np.array([])
+for i in range(0, int(percentage * dimension)):
+    randomvector = np.append(randomvector, np.random.randint(0, dimension))
+
+randomvector = sorted(randomvector)
+randomvector = np.unique(randomvector)
+vectoridx = np.setdiff1d(xrange(dimension), randomvector)
+
+print('Vector length for GP Data is: ', len(vectoridx))
+
+vectoridx = np.row_stack((vectoridx))
+
+# Sub sampling the joints inputs #
+joints = np.delete(joints, randomvector, 0)
+joints = joints.astype('float32')
+
+# Sub sampling the inertia #
+inertia = np.delete(inertia, randomvector, 0)
+inertia = inertia.astype('float32')
+
+# Sub sampling the orientation #
+orient = np.delete(orient, randomvector, 0)
+orient = orient.astype('float32')
+
+# GP Multidimensional vector
+Xp = np.column_stack((joints, inertia, orient))
+#Xp = np.column_stack((joints, inertia)) #No orientation
+
+
+#Reference and GP Velocity comparison X-Time
+matplotlib.rcParams.update({'font.size': 30, 'font.weight': 'bold'})
+fig = plt.figure(1)
+ax = fig.add_subplot(111)
+
+plt.rc('text', usetex=False)# activate latex text rendering
+reference  = sig.lfilter(filters['butter'][0], filters['butter'][1], np.row_stack((reference_velocity.getAxis(0), reference_velocity.getAxis(1), reference_velocity.getAxis(2))))
+xvelocity = reference[0,:]
+ax.plot(reference_velocity.time, xvelocity, marker='o', linestyle='-.', label="Reference Velocity", color=[0.3,0.2,0.4], lw=2)
+
+[mean1, var1] = m.predict(Xp, full_cov=True)
+xvelocity = mean1[:,0]
+xtime = np.array(reference_velocity.time)
+xtime = np.delete(xtime, randomvector, 0)
+ax.plot(xtime, xvelocity, marker='o', linestyle='-.', label="GP Velocity", color=[0,0.5,1.0], lw=2)
+
+plt.xlabel(r'X [$m$]', fontsize=35, fontweight='bold')
+plt.ylabel(r'Y [$m$]', fontsize=35, fontweight='bold')
+plt.grid(True)
+ax.legend(loc=1, prop={'size':30})
+plt.show(block=False)
+
 
