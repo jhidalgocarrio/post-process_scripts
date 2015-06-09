cf4195124221934faa4bc36418d8b38ec52610c3
Javier Hidalgo <javier.hidalgo_carrio@dfki.de>
2015-06-09 15:07:42 +0200

    [src/gaussian_process] model to reality disparity plots

--- src/gaussian_processes/wheeled_chassis_mtr_disparity.py
+++ src/gaussian_processes/wheeled_chassis_mtr_disparity.py
@@ -36,6 +36,7 @@ v = (0.5 * xx + 0.5 * yy) # linear velocity assuming both wheels have the same w
 zx = v*np.cos(rot) # velocity in x
 zy = v*np.sin(rot) # velocity in y
 z = zx + zy # resulting body velocity
+#z = sqrt(zx*zx + zy*zy) # resulting body velocity
 
 ################################################
 matplotlib.rcParams.update({'font.size': 30, 'font.weight': 'bold'})
@@ -46,7 +47,7 @@ cset = ax.contour(xx, yy, z, zdir='z', cmap=plt.cm.coolwarm,
         vmax=abs(z).max(), vmin=-abs(z).max(), linewidth=4.0)
 cset = ax.contour(xx, yy, z, zdir='x', cmap=cm.coolwarm)
 cset = ax.contour(xx, yy, z, zdir='y', cmap=cm.coolwarm)
-
+ax.set_aspect('equal')
 ax.set_xlabel('X')
 ax.set_xlim(min(xi), max(xi))
 ax.set_ylabel('Y')
@@ -57,14 +58,16 @@ plt.show(block=False)
 
 ################################################
 matplotlib.rcParams.update({'font.size': 30, 'font.weight': 'bold'})
-fig = plt.figure(2)
+fig = plt.figure(2, figsize=(10,10))
 CS = plt.contour(xx,yy,z,18)
 cbar = plt.colorbar(CS)
 cbar.ax.set_ylabel('mean value')
 # Add the contour line levels to the colorbar
 cbar.add_lines(CS)
+plt.axis('equal')
 plt.xlabel('X')
 plt.ylabel('Y')
+plt.grid(True)
 plt.show(block=False)
 
 
