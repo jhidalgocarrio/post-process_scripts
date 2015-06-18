3f2c897096bb867d9de06d4021877349633dc228
Javier Hidalgo <javier.hidalgo_carrio@dfki.de>
2015-06-18 16:02:25 +0200

    pocolog: gaussian process with new bodystate data. Verified and checked than the matrices serialization is organized in rows

--- src/pocolog/pocolog_values_gaussian_processes_new_body_state.sh
+++ src/pocolog/pocolog_values_gaussian_processes_new_body_state.sh
@@ -29,7 +29,7 @@ if [ $# -ge $NUM_ARGUMENTS ]; then
     pocolog ${args[0]}/exoter_odometry.${args[1]}.log -s /exoter_odometry.pose_samples_out --field time,angular_velocity,cov_angular_velocity > pose_odo_angular_velocity.${args[1]}.data
     echo 'Pose Odometry Samples... [DONE]'
 
-    #Matrix are organized by rows
+    #Matrix are organized by rows. Tested by comparing rock-display information and C++ std::cout
     pocolog ${args[0]}/exoter_odometry.${args[1]}.log -s /exoter_odometry.delta_pose_samples_out --field time,pose.translation,pose.cov.data[21],pose.cov.data[22],pose.cov.data[23],pose.cov.data[27],pose.cov.data[28],pose.cov.data[29],pose.cov.data[33],pose.cov.data[34],pose.cov.data[35] > delta_pose_odo_position.${args[1]}.data
     pocolog ${args[0]}/exoter_odometry.${args[1]}.log -s /exoter_odometry.delta_pose_samples_out --field time,pose.orientation,pose.cov.data[0],pose.cov.data[1],pose.cov.data[2],pose.cov.data[6],pose.cov.data[7],pose.cov.data[8],pose.cov.data[12],pose.cov.data[13],pose.cov.data[14] > delta_pose_odo_orientation.${args[1]}.data
     pocolog ${args[0]}/exoter_odometry.${args[1]}.log -s /exoter_odometry.delta_pose_samples_out --field time,velocity.vel,velocity.cov.data[21],velocity.cov.data[22],velocity.cov.data[23],velocity.cov.data[27],velocity.cov.data[28],velocity.cov.data[29],velocity.cov.data[33],velocity.cov.data[34],velocity.cov.data[35] > delta_pose_odo_velocity.${args[1]}.data
