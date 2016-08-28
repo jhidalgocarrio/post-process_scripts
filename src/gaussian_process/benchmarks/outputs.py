3fd40d8873cfef54cd36c355bc480e1b9f449c90
Javier Hidalgo <javier.hidalgo_carrio@dfki.de>
2016-08-28 16:09:46 +0200

    benchmark: improve screen output (organized by sampling times)

--- src/gaussian_process/benchmarks/outputs.py
+++ src/gaussian_process/benchmarks/outputs.py
@@ -6,6 +6,16 @@ import abc
 import os
 import numpy as np
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 class Output(object):
     __metaclass__ = abc.ABCMeta
     
@@ -17,20 +27,24 @@ class Output(object):
 class ScreenOutput(Output):
 
     def output(self, config, results):
-        print('='*20+' REPORT '+'='*20)
-        print('\t\t\t'.join([' ']+[m.name+'('+e+' sampling:'+t+')' for m in config['methods'] for e in [a.name for a in config['evaluations']] for t in config['sampling_time']]))
+        print(bcolors.BOLD + '='*20+' REPORT '+'='*20 + bcolors.ENDC)
 
         for task_i in range(len(config['tasks'])):
-            print(config['tasks'][task_i].name+'\t', end='')
+            print(bcolors.WARNING + config['tasks'][task_i].name + bcolors.ENDC)
 
-            outputs = []
             for method_i in range(len(config['methods'])):
                 for ei in range(len(config['evaluations'])):
-                    for sti in range(len(config['sampling_time'])):
-                        m = results[task_i, method_i, ei, sti].mean()
-                        t = results[task_i, method_i, -1, sti]
-                        outputs.append('%fcm [%fs]'%(m*100.0,t))
-            print('\t'.join(outputs))
+                    print(bcolors.UNDERLINE + config['methods'][method_i].name+'('+config['evaluations'][ei].name + ')'+bcolors.ENDC, end='')
+                    print(bcolors.UNDERLINE + '\t'.join([' ']+['train_sampling: '+train_t for train_t in config['train_sampling_time']]) + bcolors.ENDC)
+                    for test_ti in range(len(config['test_sampling_time'])):
+                        outputs = []
+                        for train_ti in range(len(config['train_sampling_time'])):
+                            m = results[task_i, method_i, ei, train_ti, test_ti].mean()
+                            t = results[task_i, method_i, -1, train_ti, test_ti]
+                            outputs.append('%fcm [%fs]'%(m*100.0,t))
+                        print('test_sampling: ' + config['test_sampling_time'][test_ti]+'\t', end='')
+                        print('\t\t'.join(outputs))
+                    print('\n')
 
 class CSVOutput(Output):
 
