9e22de605e929625c3a0b9090bdf641f30be9120
Javier Hidalgo <javier.hidalgo_carrio@dfki.de>
2016-08-29 09:19:55 +0200

    benchmark: update run script

--- src/gaussian_process/benchmarks/run.py
+++ src/gaussian_process/benchmarks/run.py
@@ -27,8 +27,8 @@ config = {
           'evaluations':[RMSE],
           'methods':[GP_RBF, SparseGP_RBF, GP_MAT32, SparseGP_MAT32],
           'tasks':[ExoTerOdometryResiduals],
-          'train_sampling_time':['500ms', '1s', '2s', '5s'],
-          'test_sampling_time':['500ms', '1s', '2s', '5s'],
+          'train_sampling_time':['1s', '2s', '5s'],
+          'test_sampling_time':['1s', '2s', '5s'],
           'outputs': [ScreenOutput()]
           #'outputs': [ScreenOutput(), CSVOutput(outpath, prjname)]
           }
@@ -48,30 +48,33 @@ if __name__=='__main__':
             method = config['methods'][method_i]
             print(bcolors.WARNING + 'With the method '+method.name + bcolors.ENDC)
 
+            m = method()
+
             for train_t in range(len(config['train_sampling_time'])):
                 train_time = config['train_sampling_time'][train_t]
                 print(bcolors.BOLD + 'Train sampling time: '+ bcolors.ENDC + bcolors.WARNING + train_time + bcolors.ENDC)
+
                 train = dataset.get_training_data(train_time)
+                t_st = time.time()
+                m.fit(train)
+                t_pd = time.time() - t_st
 
                 for test_t in range(len(config['test_sampling_time'])):
                     test_time = config['test_sampling_time'][test_t]
                     print(bcolors.BOLD + 'Test sampling time: '+ bcolors.ENDC + bcolors.WARNING + test_time + bcolors.ENDC)
-                    test = dataset.get_test_data(test_time)
 
-                    m = method()
-                    t_st = time.time()
-                    m.fit(train)
+                    test = dataset.get_test_data(test_time)
                     pred_mean = m.predict(test[0])
-                    t_pd = time.time() - t_st
 
                     print (bcolors.OKGREEN + "Prediction shape" + str(pred_mean.shape) + bcolors.ENDC)
 
                     for ei in range(len(config['evaluations'])):
                         evalu = config['evaluations'][ei]()
                         print(bcolors.OKBLUE + 'With evaluation method '+evalu.name + bcolors.ENDC)
-                        results[task_i, method_i, ei, train_t, test_t] = evalu.evaluate(test[1], pred_mean)
-                    results[task_i, method_i, -1, train_t, test_t] = t_pd
 
+                        results[task_i, method_i, ei, train_t, test_t] = evalu.evaluate(test[1], pred_mean)
+
+                    results[task_i, method_i, -1, train_t, test_t] = t_pd
                     figure = ExoTerFigures()
                     figure.output(fig_num, dataset, m, pred_mean, 0, train_time, test_time)
                     fig_num = fig_num + 1
