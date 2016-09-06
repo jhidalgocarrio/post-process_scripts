ae5a16d2b48c66782d004bc06775bf5f7217d400
Javier Hidalgo <javier.hidalgo_carrio@dfki.de>
2016-09-06 20:39:53 +0200

    bash: update orb_slam2 pocolog script

--- src/bash/pocolog_values_orb_slam2.sh
+++ src/bash/pocolog_values_orb_slam2.sh
@@ -7,10 +7,14 @@ NUM_ARGUMENTS=2
 if [ $# -ge $NUM_ARGUMENTS ]; then
     echo Running the pocolog commands for the directory: ${args[0]} for *.${args[1]}.log files
 
-    pocolog ${args[0]}/orb_slam2.${args[1]}.log -s  /orb_slam2.keyframe_trajectory_out --field [0].position,[0].heading  > trajectory_keyframe_orb_slam2_pose.${args[1]}.data
-    #pocolog ${args[0]}/orb_slam2.${args[1]}.log -s /orb_slam2.keyframe_pose_samples_out --field time,position,cov_position > pose_keyframe_orb_slam2_position.${args[1]}.data
-    pocolog ${args[0]}/orb_slam2.${args[1]}.log -s /orb_slam2.pose_samples_out --field time,position,cov_position > pose_keyframe_orb_slam2_position.${args[1]}.data
+    pocolog ${args[0]}/orb_slam2.${args[1]}.log -s /orb_slam2.keyframe_pose_samples_out --field time,position,cov_position > pose_keyframe_orb_slam2_position.${args[1]}.data
+    pocolog ${args[0]}/orb_slam2.${args[1]}.log -s /orb_slam2.keyframe_pose_samples_out --field time,orientation,cov_orientation > pose_keyframe_orb_slam2_orientation.${args[1]}.data
+    pocolog ${args[0]}/orb_slam2.${args[1]}.log -s /orb_slam2.pose_samples_out --field time,position,cov_position > pose_orb_slam2_position.${args[1]}.data
+    pocolog ${args[0]}/orb_slam2.${args[1]}.log -s /orb_slam2.pose_samples_out --field time,orientation,cov_orientation > pose_orb_slam2_orientation.${args[1]}.data
     echo 'SLAM Pose Samples... [DONE]'
+
+    pocolog ${args[0]}/orb_slam2.${args[1]}.log -s /orb_slam2.task_info_out  > task_orb_slam2_info.data.${args[1]}.data
+    echo 'Task information... [DONE]'
 
     pocolog ${args[0]}/localization_frontend.${args[1]}.log -s /localization_frontend.pose_reference_samples_out --field time,position,cov_position > pose_ref_position.${args[1]}.data
     pocolog ${args[0]}/localization_frontend.${args[1]}.log -s /localization_frontend.pose_reference_samples_out --field time,orientation,cov_orientation > pose_ref_orientation.${args[1]}.data
@@ -30,6 +34,10 @@ if [ $# -ge $NUM_ARGUMENTS ]; then
     pocolog ${args[0]}/gpy_gp_odometry.${args[1]}.log -s /gpy_gp_odometry.delta_pose_samples_out --field time,position,cov_position > delta_pose_odo_position.${args[1]}.data
     pocolog ${args[0]}/gpy_gp_odometry.${args[1]}.log -s /gpy_gp_odometry.delta_pose_samples_out --field time,orientation,cov_orientation > delta_pose_odo_orientation.${args[1]}.data
     echo 'GP-Odometry Delta Pose Samples... [DONE]'
+
+    pocolog ${args[0]}/localization_frontend.${args[1]}.log -s /localization_frontend.world_to_navigation_out --field time,position,cov_position > pose_world_to_navigation_position.${args[1]}.data
+    pocolog ${args[0]}/localization_frontend.${args[1]}.log -s /localization_frontend.world_to_navigation_out --field time,orientation,cov_orientation > pose_world_to_navigation_orientation.${args[1]}.data
+    echo 'World to Navigation Samples... [DONE]'
 
     # Remove first two lines of the files (pocolog headers)
     sed -i '1,2d' *.data
