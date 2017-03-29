3d86deaf7c0d87d61adbef07b9d2a90055e4a919
Javier Hidalgo-Carrio <jhidalgocarrio@ieee.org>
2017-03-29 10:47:38 +0200

    exoter_odometry: tables and values for decos 20140911-1805 with pandas.

--- src/exoter_odometry/decos_position_tables_20140911-1805.py
+++ src/exoter_odometry/decos_position_tables_20140911-1805.py
@@ -4,11 +4,11 @@
 # Decos Terrain Data #
 # ################## #
 
-path = '/home/javi/exoter/development/data/20140911_decos_field/20140911-1805_odometry_comparison_bis/'
+path = '~/npi/data/20140911_decos_field/20140911-1805_odometry_comparison_bis/'
 #######################################
-odometry_file = path + 'pose_odo_position.reaction_forces.0.data'
+threed_odometry_file = path + 'pose_odo_position.reaction_forces.0.data'
 
-skid_file = path + 'pose_skid_position.0.data'
+skid_odometry_file = path + 'pose_skid_position.0.data'
 
 reference_file = path + 'pose_ref_position.0.data'
 #######################################
@@ -19,294 +19,208 @@ pose_odo_orient_file = path + "pose_odo_orientation.reaction_forces.0.data"
 pose_ref_orient_file = path + "pose_ref_orientation.0.data"
 #######################################
 
-
 import sys
 sys.path.insert(0, './src/core')
-import datadisplay as data
-from random import gauss
-import csv, scipy
-import math
+import os
+import csv
 from pylab import *
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.patches import Ellipse
 import quaternion as quat
-from math import sqrt
+import datadisplay as data
+import cov_ellipse as cov
+from plyfile import PlyData, PlyElement
+import scipy
+from numpy import linalg as la
+
+import pandas as pandas
+import datetime
+matplotlib.style.use('ggplot') #in matplotlib >= 1.5.1
+
+def dateparse (time_in_microsecs):
+    return datetime.datetime.fromtimestamp(float(time_in_microsecs) * 1e-06)
+
+
+##########################################################################
+# READ THE VALUES IN PANDAS
+##########################################################################
 
 #ExoTeR Odometry
-threed_odometry = data.ThreeData()
-threed_odometry.readData(odometry_file, cov=True)
-
-# Read the odometry orientation information
-odometry_orient = data.QuaternionData()
-odometry_orient.readData(pose_odo_orient_file, cov=True)
+threed_odometry = pandas.read_csv(os.path.expanduser(threed_odometry_file), sep=" ", parse_dates=True,
+    date_parser=dateparse , index_col='time',
+    names=['time', 'x', 'y', 'z', 'cov_xx', 'cov_xy', 'cov_xz', 'cov_yx', 'cov_yy', 'cov_yz',
+        'cov_zx', 'cov_zy', 'cov_zz'], header=None)
 
 #Skid Odometry
-skid_odometry = data.ThreeData()
-skid_odometry.readData(skid_file, cov=True)
+skid_odometry = pandas.read_csv(os.path.expanduser(skid_odometry_file), sep=" ", parse_dates=True,
+    date_parser=dateparse , index_col='time',
+    names=['time', 'x', 'y', 'z', 'cov_xx', 'cov_xy', 'cov_xz', 'cov_yx', 'cov_yy', 'cov_yz',
+        'cov_zx', 'cov_zy', 'cov_zz'], header=None)
 
-#GNSS Pose
-reference = data.ThreeData()
-reference.readData(reference_file, cov=True)
+#Vicon Pose
+reference = pandas.read_csv(os.path.expanduser(reference_file), sep=" ", parse_dates=True,
+    date_parser=dateparse , index_col='time',
+    names=['time', 'x', 'y', 'z', 'cov_xx', 'cov_xy', 'cov_xz', 'cov_yx', 'cov_yy', 'cov_yz',
+        'cov_zx', 'cov_zy', 'cov_zz'], header=None)
 
-#GNSS Delta Pose
-delta_reference = data.ThreeData()
-delta_reference.readData(delta_reference_file, cov=True)
+#Vicon Delta Pose
+delta_reference = pandas.read_csv(os.path.expanduser(delta_reference_file), sep=" ", parse_dates=True,
+    date_parser=dateparse , index_col='time',
+    names=['time', 'x', 'y', 'z', 'cov_xx', 'cov_xy', 'cov_xz', 'cov_yx', 'cov_yy', 'cov_yz',
+        'cov_zx', 'cov_zy', 'cov_zz'], header=None)
 
-# Read the reference orientation information
-reference_orient = data.QuaternionData()
-reference_orient.readData(pose_ref_orient_file, cov=True)
+##########################################################################
+odometry_mask = pandas.notnull(threed_odometry.x) & pandas.notnull(threed_odometry.y) & pandas.notnull(threed_odometry.z)
+threed_odometry = threed_odometry[odometry_mask]
 
+skid_mask = pandas.notnull(skid_odometry.x) & pandas.notnull(skid_odometry.y) & pandas.notnull(skid_odometry.z)
+skid_odometry = skid_odometry[skid_mask]
 
-########################
-### REMOVE OUTLIERS  ###
-########################
-temindex = np.where(np.isnan(reference.cov[:,0,0]))
-temindex = np.asarray(temindex)
+ref_mask = pandas.notnull(reference.x) & pandas.notnull(reference.y) & pandas.notnull(reference.z)
+reference = reference[ref_mask]
 
-reference.delete(temindex)
-threed_odometry.delete(temindex)
-skid_odometry.delete(temindex)
-
-#######################################
-### COMPUTE COVARIANCE EIGENVALUES  ###
-#######################################
-reference.eigenValues()
-threed_odometry.eigenValues()
-skid_odometry.eigenValues()
+delta_ref_mask = pandas.notnull(delta_reference.x) & pandas.notnull(delta_reference.y) & pandas.notnull(delta_reference.z)
+delta_reference = delta_reference[delta_ref_mask]
 
 ###########################
 ##   Distance traveled   ##
 ###########################
-# No use the z axis when using
-# reference because GPS is really bad in Z
-# direction
-position = np.column_stack((delta_reference.getAxis(0),
-    delta_reference.getAxis(1), delta_reference.getAxis(1)))
+# No use the z axis when using reference because GPS is really bad in Z direction
+
+position = np.column_stack((delta_reference.x.values, delta_reference.y.values))
 
 norm_delta_position = []
 norm_delta_position = [np.linalg.norm(x) for x in position ]
+norm_delta_position = [x for x in norm_delta_position if str(x) != 'inf']
 distance_traveled = np.nansum(norm_delta_position)
 
 #################################################
-# Take the misalignment between both orientations
+mis_orient = quat.quaternion.fromAngleAxis(5.0 * np.pi/180.0, [0.0, 0.0, 1.0])
+mis_position = [0.0, 0.0, 0.00]
 #################################################
-#misalignment = ~odometry_orient.data[1000] * reference_orient.data[1000] #~ is invert operator
-misalignment = quat.quaternion(np.array([1.0,0.0,0.0,0.0]))
+
+#################
+## RE-SAMPLE   ##
+#################
+resampling_time = '1s'
+threed_odometry = threed_odometry.resample(resampling_time).mean()
+skid_odometry = skid_odometry.resample(resampling_time).mean()
+reference = reference.resample(resampling_time).mean()
 
 #################################################
-# Align odometry with the staring orientation of 
-# the GNSS information 
+threed_pose = np.array([(mis_orient * i * mis_orient.conj())[1:4] for i in np.column_stack((threed_odometry.x.values, threed_odometry.y.values, threed_odometry.z.values))])
+threed_pose[:] = [ i + mis_position for i in threed_pose ]
+threed_odometry.x = threed_pose[:,0]
+threed_odometry.y = threed_pose[:,1]
+threed_odometry.z = threed_pose[:,2]
+
 #################################################
-odopos = np.column_stack((threed_odometry.getAxis(0), threed_odometry.getAxis(1),
-        threed_odometry.getAxis(2)))
-odopos[:] = [(misalignment * x * misalignment.conj())[1:4] for x in odopos ]
+skid_pose = np.array([(mis_orient * i * mis_orient.conj())[1:4] for i in np.column_stack((skid_odometry.x.values, skid_odometry.y.values, skid_odometry.z.values))])
+skid_pose[:] = [ i + mis_position for i in skid_pose ]
+skid_odometry.x = skid_pose[:,0]
+skid_odometry.y = skid_pose[:,1]
+skid_odometry.z = skid_pose[:,2]
 
-###################
-###    MEAN     ###
-###################
-numbersamples=200
-#Create a mean value to synchronize the trajectories
-odotime = []
-odoposx = []
-odoposy = []
-odoposz = []
-for i in range(0, len(threed_odometry.getAxis(0)), numbersamples):
-    odotime.append(mean(threed_odometry.t[0+i:numbersamples+i]))
-    odoposx.append(mean(odopos[0+i:numbersamples+i,0]))
-    odoposy.append(mean(odopos[0+i:numbersamples+i,1]))
-    odoposz.append(mean(odopos[0+i:numbersamples+i,2]))
-
-odopos=[]
-odopos.append(np.array(odoposx))
-odopos.append(np.array(odoposy))
-odopos.append(np.array(odoposz))
-
-del(odoposx, odoposy, odoposz)
-
-#Create a mean value to synchronize the trajectories
-skidtime = []
-skidposx = []
-skidposy = []
-skidposz = []
-for i in range(0, len(skid_odometry.getAxis(0)), numbersamples):
-    skidtime.append(mean(skid_odometry.t[0+i:numbersamples+i]))
-    skidposx.append(mean(skid_odometry.getAxis(0)[0+i:numbersamples+i]))
-    skidposy.append(mean(skid_odometry.getAxis(1)[0+i:numbersamples+i]))
-    skidposz.append(mean(skid_odometry.getAxis(2)[0+i:numbersamples+i]))
-
-
-skidpos=[]
-skidpos.append(np.array(skidposx))
-skidpos.append(np.array(skidposy))
-skidpos.append(np.array(skidposz))
-
-del(skidposx, skidposy, skidposz)
-
-#Create a mean value to synchronize the trajectories
-reftime = []
-refposx = []
-refposy = []
-refposz = []
-for i in range(0, len(reference.getAxis(0)), numbersamples):
-    reftime.append(mean(reference.t[0+i:numbersamples+i]))
-    refposx.append(mean(reference.getAxis(0)[0+i:numbersamples+i]))
-    refposy.append(mean(reference.getAxis(1)[0+i:numbersamples+i]))
-    refposz.append(mean(reference.getAxis(2)[0+i:numbersamples+i]))
-
-
-refpos=[]
-refpos.append(np.array(refposx))
-refpos.append(np.array(refposy))
-refpos.append(np.array(refposz))
-
-del(refposx, refposy, refposz)
-
+#################################################
 # Plot to verify the data to analyze
-plot(odopos[0], odopos[1], color='blue')
-plot(skidpos[0], skidpos[1], color='red')
-plot(refpos[0], refpos[1], color='black')
+plot(threed_odometry.x.values, threed_odometry.y.values, color='blue')
+plot(skid_odometry.x.values, skid_odometry.y.values, color='red')
+plot(reference.x.values, reference.y.values, color='black')
 plt.xlabel(r'Position in X [$m$]', fontsize=24)
 plt.ylabel(r'Position in Y [$m$]', fontsize=24)
 plt.grid(True)
 plt.legend(prop={'size':25})
-plt.show(block=False)
+plt.show(block=True)
 
-plot(odopos[0], odopos[2], color='blue')
-plot(skidpos[0], skidpos[2], color='red')
-plot(refpos[0], refpos[2], color='black')
-plt.xlabel(r'Position in X [$m$]', fontsize=24)
-plt.ylabel(r'Position in Z [$m$]', fontsize=24)
-plt.grid(True)
-plt.legend(prop={'size':25})
-plt.show(block=False)
-
-#Plot over  time
-plot(odotime, odopos[0], color='blue',  marker='x')
-plot(skidtime, skidpos[0], color='red', marker='x')
-plot(reftime, refpos[0], color='black', marker='x')
-plt.xlabel(r'Time  [$s$]', fontsize=24)
-plt.ylabel(r'Position in X [$m$]', fontsize=24)
-plt.grid(True)
-plt.legend(prop={'size':25})
-plt.show(block=False)
-
-plot(odotime, odopos[1])
-plot(skidtime, skidpos[1], color='red')
-plot(reftime, refpos[1], color='black')
-plt.xlabel(r'Time  [$s$]', fontsize=24)
-plt.ylabel(r'Position in Y [$m$]', fontsize=24)
-plt.grid(True)
-plt.legend(prop={'size':25})
-plt.show(block=False)
-
-plot(odotime, odopos[2])
-plot(skidtime, skidpos[2], color='red')
-plot(reftime, refpos[2], color='black')
-plt.xlabel(r'Time  [$s$]', fontsize=24)
-plt.ylabel(r'Position in Z [$m$]', fontsize=24)
-plt.grid(True)
-plt.legend(prop={'size':25})
-plt.show(block=False)
-
-# Plot error over time
-datasize=[]
-datasize.append(min(len(odopos[0]), len(refpos[0])))
-datasize.append(min(len(odopos[1]), len(refpos[1])))
-datasize.append(min(len(odopos[2]), len(refpos[2])))
-odoerrorx = np.absolute(odopos[0][0:datasize[0]] - refpos[0][0+3:datasize[0]+3])
-odoerrory = np.absolute(odopos[1][0:datasize[0]] - refpos[1][0+3:datasize[0]+3])
-odoerrorz = np.absolute(odopos[2][0:datasize[0]] - refpos[2][0+3:datasize[0]+3])
-
-plot(odotime, odoerrorx, marker='o')
-plt.xlabel(r'Time  [$s$]', fontsize=24)
-plt.ylabel(r'Position Error in X [$m$]', fontsize=24)
-plt.grid(True)
-plt.legend(prop={'size':25})
-plt.show(block=False)
-
+# Compute the 3d odometry error
+threed_odometry['error_x'] = pandas.Series (fabs(threed_odometry.x - reference.x))
+threed_odometry['error_y'] = pandas.Series (fabs(threed_odometry.y - reference.y))
+threed_odometry['error_z'] = pandas.Series (fabs(threed_odometry.z - reference.z))
+odometry_mask = pandas.notnull(threed_odometry.error_x) & pandas.notnull(threed_odometry.error_y) & pandas.notnull(threed_odometry.error_z)
+threed_odometry = threed_odometry[odometry_mask]
 
 # RMSE for Full Odometry
-datasize=[]
-datasize.append(min(len(odopos[0]), len(refpos[0])))
-datasize.append(min(len(odopos[1]), len(refpos[1])))
-datasize.append(min(len(odopos[2]), len(refpos[2])))
 odormse = []
-odormse.append(sqrt(((odopos[0][0:datasize[0]] - refpos[0][0:datasize[0]]) ** 2).mean()))
-odormse.append(sqrt(((odopos[1][0:datasize[1]] - refpos[1][0:datasize[1]]) ** 2).mean()))
-odormse.append(sqrt(((odopos[2][0:datasize[2]] - refpos[2][0:datasize[2]]) ** 2).mean()))
+odormse.append(sqrt(((threed_odometry.error_x.values) ** 2).mean()))
+odormse.append(sqrt(((threed_odometry.error_y.values) ** 2).mean()))
+odormse.append(sqrt(((threed_odometry.error_z.values) ** 2).mean()))
 
 #Final error position for Full Odometry
 odofinale = []
-odofinale.append(np.absolute(odopos[0][len(odopos[0])-1] - refpos[0][len(refpos[0])-1]))
-odofinale.append(np.absolute(odopos[1][len(odopos[2])-1] - refpos[1][len(refpos[1])-1]))
-odofinale.append(np.absolute(odopos[2][len(odopos[2])-1] - refpos[2][len(refpos[2])-1]))
+odofinale.append(np.absolute(threed_odometry.error_x.values[threed_odometry.shape[0]-1]))
+odofinale.append(np.absolute(threed_odometry.error_y.values[threed_odometry.shape[0]-1]))
+odofinale.append(np.absolute(threed_odometry.error_z.values[threed_odometry.shape[0]-1]))
 
 # Maximum error for Full Odometry
-datasize=[]
-datasize.append(min(len(odopos[0]), len(refpos[0])))
-datasize.append(min(len(odopos[1]), len(refpos[1])))
-datasize.append(min(len(odopos[2]), len(refpos[2])))
 odomaxe = []
-odomaxe.append(np.absolute(odopos[0][0:datasize[0]] - refpos[0][0:datasize[0]]).max())
-odomaxe.append(np.absolute(odopos[1][0:datasize[1]] - refpos[1][0:datasize[1]]).max())
-odomaxe.append(np.absolute(odopos[2][0:datasize[2]] - refpos[2][0:datasize[2]]).max())
+odomaxe.append(threed_odometry.error_x.values.max())
+odomaxe.append(threed_odometry.error_y.values.max())
+odomaxe.append(threed_odometry.error_z.values.max())
 
 # Median error for Full Odometry
-datasize=[]
-datasize.append(min(len(odopos[0]), len(refpos[0])))
-datasize.append(min(len(odopos[1]), len(refpos[1])))
-datasize.append(min(len(odopos[2]), len(refpos[2])))
 odomediane = []
-odomediane.append(np.median(np.absolute(odopos[0][0:datasize[0]] - refpos[0][0:datasize[0]])))
-odomediane.append(np.median(np.absolute(odopos[1][0:datasize[1]] - refpos[1][0:datasize[1]])))
-odomediane.append(np.median(np.absolute(odopos[2][0:datasize[2]] - refpos[2][0:datasize[2]])))
+odomediane.append(np.median(np.absolute(threed_odometry.error_x.values)))
+odomediane.append(np.median(np.absolute(threed_odometry.error_y.values)))
+odomediane.append(np.median(np.absolute(threed_odometry.error_z.values)))
+
+# Compute the skid odometry error
+skid_odometry['error_x'] = pandas.Series (fabs(skid_odometry.x - reference.x))
+skid_odometry['error_y'] = pandas.Series (fabs(skid_odometry.y - reference.y))
+skid_odometry['error_z'] = pandas.Series (fabs(skid_odometry.z - reference.z))
+skid_mask = pandas.notnull(skid_odometry.error_x) & pandas.notnull(skid_odometry.error_y) & pandas.notnull(skid_odometry.error_z)
+skid_odometry = skid_odometry[skid_mask]
 
 # RMSE for Skid Odometry
-datasize=[]
-datasize.append(min(len(skidpos[0]), len(refpos[0])))
-datasize.append(min(len(skidpos[1]), len(refpos[1])))
-datasize.append(min(len(skidpos[2]), len(refpos[2])))
 skidrmse = []
-skidrmse.append(sqrt(((skidpos[0][0:datasize[0]] - refpos[0][0:datasize[0]]) ** 2).mean()))
-skidrmse.append(sqrt(((skidpos[1][0:datasize[1]] - refpos[1][0:datasize[1]]) ** 2).mean()))
-skidrmse.append(sqrt(((skidpos[2][0:datasize[2]] - refpos[2][0:datasize[2]]) ** 2).mean()))
+skidrmse.append(sqrt(((skid_odometry.error_x.values) ** 2).mean()))
+skidrmse.append(sqrt(((skid_odometry.error_y.values) ** 2).mean()))
+skidrmse.append(sqrt(((skid_odometry.error_z.values) ** 2).mean()))
 
 #Final error position for Skid Odometry
 skidfinale = []
-skidfinale.append(np.absolute(skidpos[0][len(skidpos[0])-1] - refpos[0][len(refpos[0])-1]))
-skidfinale.append(np.absolute(skidpos[1][len(skidpos[1])-1] - refpos[1][len(refpos[1])-1]))
-skidfinale.append(np.absolute(skidpos[2][len(skidpos[2])-1] - refpos[2][len(refpos[2])-1]))
+skidfinale.append(np.absolute(skid_odometry.error_x.values[skid_odometry.shape[0]-1]))
+skidfinale.append(np.absolute(skid_odometry.error_y.values[skid_odometry.shape[0]-1]))
+skidfinale.append(np.absolute(skid_odometry.error_z.values[skid_odometry.shape[0]-1]))
 
 # Maximum error for Skid Odometry
-datasize=[]
-datasize.append(min(len(skidpos[0]), len(refpos[0])))
-datasize.append(min(len(skidpos[1]), len(refpos[1])))
-datasize.append(min(len(skidpos[2]), len(refpos[2])))
 skidmaxe = []
-skidmaxe.append(np.absolute(skidpos[0][0:datasize[0]] - refpos[0][0:datasize[0]]).max())
-skidmaxe.append(np.absolute(skidpos[1][0:datasize[1]] - refpos[1][0:datasize[1]]).max())
-skidmaxe.append(np.absolute(skidpos[2][0:datasize[2]] - refpos[2][0:datasize[2]]).max())
+skidmaxe.append(skid_odometry.error_x.values.max())
+skidmaxe.append(skid_odometry.error_y.values.max())
+skidmaxe.append(skid_odometry.error_z.values.max())
 
 # Median error for Skid Odometry
-datasize=[]
-datasize.append(min(len(skidpos[0]), len(refpos[0])))
-datasize.append(min(len(skidpos[1]), len(refpos[1])))
-datasize.append(min(len(skidpos[2]), len(refpos[2])))
 skidmediane = []
-skidmediane.append(np.median(np.absolute(skidpos[0][0:datasize[0]] - refpos[0][0:datasize[0]])))
-skidmediane.append(np.median(np.absolute(skidpos[1][0:datasize[1]] - refpos[1][0:datasize[1]])))
-skidmediane.append(np.median(np.absolute(skidpos[2][0:datasize[2]] - refpos[2][0:datasize[2]])))
+skidmediane.append(np.median(np.absolute(skid_odometry.error_x.values)))
+skidmediane.append(np.median(np.absolute(skid_odometry.error_y.values)))
+skidmediane.append(np.median(np.absolute(skid_odometry.error_z.values)))
+
+# RMSE for Reference (it shoudl be zero)
+
+from numpy import linalg as la
 
 # Print values
-odormse
-skidrmse
+print("3d odometry rmse:" + str(la.norm(odormse)))
+print("skid odomery rmse:" + str(la.norm(skidrmse)))
 
-odomaxe
-skidmaxe
+print("3d odometry max error:" + str(la.norm(odomaxe)))
+print("skid odomery max error:" + str(la.norm(skidmaxe)))
 
-odomediane
-skidmediane
+print("3d odometry median error:" + str(la.norm(odomediane)))
+print("skid odomery median error:" + str(la.norm(skidmediane)))
 
-odofinale
-skidfinale
+print("3d odometry final error:" + str(la.norm(odofinale)))
+print("skid odomery median error:" + str(la.norm(skidfinale)))
 
+# Print values (in x-y)
+print("3d odometry rmse:" + str(la.norm(odormse[0:2])))
+print("skid odomery rmse:" + str(la.norm(skidrmse[0:2])))
+
+print("3d odometry max error:" + str(la.norm(odomaxe[0:2])))
+print("skid odomery max error:" + str(la.norm(skidmaxe[0:2])))
+
+print("3d odometry median error:" + str(la.norm(odomediane[0:2])))
+print("skid odomery median error:" + str(la.norm(skidmediane[0:2])))
+
+print("3d odometry final error:" + str(la.norm(odofinale[0:2])))
+print("skid odomery median error:" + str(la.norm(skidfinale[0:2])))
