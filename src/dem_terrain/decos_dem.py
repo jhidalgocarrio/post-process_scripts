84e1f15ca797f71769d92fbfeea4752c578a1d01
Javier Hidalgo-Carrio <jhidalgocarrio@ieee.org>
2017-06-15 18:50:02 +0200

    dem_terrain: update scripts for arl and decos dem plots.

--- src/dem_terrain/decos_dem.py
+++ src/dem_terrain/decos_dem.py
@@ -2,21 +2,25 @@
 
 #######################################
 #decos_dem_file = '/home/javi/exoter/development/decos_terrain/2014_10_17_test_geotag_filtered_segmented_point_cloud.ply'
-decos_dem_file = '/home/javi/exoter/development/decos_terrain/decos_small_testing_area_point_cloud.ply'
+decos_dem_file = '~/npi/documentation/decos_terrain/decos_small_testing_area_point_cloud.ply'
 #######################################
 
 import sys
+import os
 sys.path.insert(0, './src/core')
 from numpy.random import uniform, seed
 from matplotlib.mlab import griddata
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import axes3d
 from matplotlib import cm
+from pylab import *
 import numpy as np
 from plyfile import PlyData, PlyElement
 import scipy
 
-plydata = PlyData.read(open(decos_dem_file))
+matplotlib.style.use('classic') #in matplotlib >= 1.5.1
+
+plydata = PlyData.read(open(os.path.expanduser(decos_dem_file)))
 
 vertex = plydata['vertex'].data
 
@@ -33,7 +37,7 @@ zi = griddata(px, py, pz, xi, yi, interp='linear')
 ############
 ### PLOT ###
 ############
-fig = plt.figure(1)
+fig = plt.figure(1, figsize=(28, 16), dpi=120, facecolor='w', edgecolor='k')
 
 plt.rc('text', usetex=False)# activate latex text rendering
 levs = np.arange(np.floor(zi.min()), np.ceil(zi.max()), 0.1)
@@ -51,14 +55,15 @@ plt.show(block=False)
 x, y = np.meshgrid(xi, yi)
 
 ############
-fig = plt.figure()
+fig = plt.figure(2, figsize=(28, 16), dpi=120, facecolor='w', edgecolor='k')
 ax = fig.gca(projection='3d')
 surf = ax.plot_surface(x, y, zi, rstride=15, cstride=15, cmap=cm.coolwarm, linewidth=1, antialiased=True)
 fig.colorbar(surf)
 
 plt.show()
 
-fig = plt.figure()
+############
+fig = plt.figure(3, figsize=(28, 16), dpi=120, facecolor='w', edgecolor='k')
 ax = fig.gca(projection='3d')
 ax.plot_surface(x, y, zi, rstride=2, cstride=2, alpha=1.0, linewidth=0.1, cmap=plt.cm.gray, vmax=abs(zi).max(), vmin=-abs(zi).max())
 cset = ax.contour(x, y, zi, zdir='z', cmap=plt.cm.coolwarm,
@@ -66,14 +71,15 @@ cset = ax.contour(x, y, zi, zdir='z', cmap=plt.cm.coolwarm,
 cset = ax.contour(x, y, zi, zdir='x', cmap=plt.cm.gray, vmax=abs(xi).max(), vmin=-abs(xi).max())
 cset = ax.contour(x, y, zi, zdir='y', cmap=plt.cm.gray, vmax=abs(yi).max(), vmin=-abs(yi).max())
 
-ax.set_xlabel('X')
+ax.set_xlabel('\n\nX [$m$]', fontsize=35)
 ax.set_xlim(0, max(xi))
-ax.set_ylabel('Y')
+ax.set_ylabel('\n\nY [$m$]', fontsize=35)
 ax.set_ylim(0, max(yi))
-ax.set_zlabel('Z')
+ax.set_zlabel('\n\nZ [$m$]', fontsize=35)
 ax.set_zlim(0, 10)
 
 plt.show(block=False)
+fig.savefig("decos_dem_3d_terrain.pdf", dpi=fig.dpi)
 
 
 
--- src/dem_terrain/esa_arl_dem.py
+++ src/dem_terrain/esa_arl_dem.py
@@ -1,21 +1,26 @@
 #!/usr/bin/env python
 
 #######################################
-esa_arl_dem_file = '/home/javi/exoter/development/esa_terrain_lab/DEMclean.ply'
+esa_arl_dem_file = '~/npi/documentation/esa_terrain_lab/DEMclean.ply'
 #######################################
 
 import sys
+import os
 sys.path.insert(0, './src/core')
 from numpy.random import uniform, seed
 from matplotlib.mlab import griddata
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import axes3d
 from matplotlib import cm
+from pylab import *
 import numpy as np
 from plyfile import PlyData, PlyElement
 import scipy
 
-plydata = PlyData.read(open(esa_arl_dem_file))
+matplotlib.style.use('classic') #in matplotlib >= 1.5.1
+matplotlib.rcParams['xtick.labelsize'] = 30
+
+plydata = PlyData.read(open(os.path.expanduser(esa_arl_dem_file)))
 
 vertex = plydata['vertex'].data
 
@@ -32,7 +37,7 @@ zi = griddata(px, py, pz, xi, yi, interp='linear')
 ############
 ### PLOT ###
 ############
-fig = plt.figure(1)
+fig = plt.figure(1, figsize=(28, 16), dpi=120, facecolor='w', edgecolor='k')
 
 plt.rc('text', usetex=False)# activate latex text rendering
 CS = plt.contour(xi, yi, zi, 15, linewidths=0.5, colors='k')
@@ -49,14 +54,15 @@ plt.show(block=False)
 x, y = np.meshgrid(xi, yi)
 
 ############
-fig = plt.figure()
+fig = plt.figure(2, figsize=(28, 16), dpi=120, facecolor='w', edgecolor='k')
 ax = fig.gca(projection='3d')
 surf = ax.plot_surface(x, y, zi, rstride=15, cstride=15, cmap=cm.coolwarm, linewidth=1, antialiased=True)
 fig.colorbar(surf)
 
 plt.show()
 
-fig = plt.figure()
+############
+fig = plt.figure(3, figsize=(28, 16), dpi=120, facecolor='w', edgecolor='k')
 ax = fig.gca(projection='3d')
 ax.plot_surface(x, y, zi, rstride=2, cstride=2, alpha=1.0, linewidth=0.3, cmap=plt.cm.gray, vmax=abs(zi).max(), vmin=-abs(zi).max())
 cset = ax.contour(x, y, zi, zdir='z', cmap=plt.cm.gist_earth,
@@ -64,15 +70,15 @@ cset = ax.contour(x, y, zi, zdir='z', cmap=plt.cm.gist_earth,
 cset = ax.contour(x, y, zi, zdir='x', cmap=plt.cm.gray, vmax=abs(xi).max(), vmin=-abs(xi).max())
 cset = ax.contour(x, y, zi, zdir='y', cmap=plt.cm.gray, vmax=abs(yi).max(), vmin=-abs(yi).max())
 
-ax.set_xlabel('X')
+ax.set_xlabel('\n\nX [$m$]', fontsize=35)
 ax.set_xlim(0, max(xi))
-ax.set_ylabel('Y')
+ax.set_ylabel('\n\nY [$m$]', fontsize=35)
 ax.set_ylim(0, max(yi))
-ax.set_zlabel('Z')
+ax.set_zlabel('\n\nZ [$m$]', fontsize=35)
 ax.set_zlim(0, 4)
 
 plt.show(block=False)
-
+fig.savefig("arl_dem_3d_terrain.pdf", dpi=fig.dpi)
 
 
 
